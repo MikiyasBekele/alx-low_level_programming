9482a993f89140299c3e75500be4c387dd607663
MikiyasBekele <mikiyas12getachew@gmail.com>
2023-02-16 04:17:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  * main - A program that prints a line with puts function
  * Return: 0 (success)
  */		
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle"");
-	return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"");
+return (0);
 }

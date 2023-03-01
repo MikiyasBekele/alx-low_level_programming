928ef84bb6990bbbb722d202a8a5c08de04815f3
MikiyasBekele <mikiyas12getachew@gmail.com>
2023-03-01 06:05:53 -0800

    task11

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+/**
+ * write your line of code here...
+ * Remember:
+ * You are not allowed to use the variable a in your new line of code here
+ * You are not allowed to use the variable a in your new line of code
+ */
+
+(p + 5) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
